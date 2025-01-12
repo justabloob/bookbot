c6969bc31503a8989247015afa6ce27069485c3d
justabloob <justabloob@gmail.com>
2025-01-12 18:30:33 +0100

    adding a line count to the fuction

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 def main():
+    line_count = 0
     with open("books/frankenstein.txt") as f:
         for line in f:
             print(line.strip())
-
+            line_count += 1
+    print(f"Line count: {line_count}")
 main()
