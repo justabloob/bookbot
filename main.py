b8b52abfbc2a3e797fc4fac6330d64e4bb493105
justabloob <justabloob@gmail.com>
2025-01-12 19:41:40 +0100

    added count words function, and more comments

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 def main():
     book_path = "books/frankenstein.txt" # stablish the path to the book
     text = get_book_text(book_path) # get the text from the book
-    print(text)
+    word_count = count_words(text) # count the words in the text
+    print(f"Word count: {word_count}")
 
 def get_book_text(book_path): # function to get the text from the book
     with open(book_path) as f: # f is our opened file
         return f.read() # read() gets all the text and returns it
 
-# is better to have two functions, following the separation of concerns principle
-# this way, we can test the get_book_text function separately from the main function
-# and we can reuse the get_book_text function if we need to get the text from another book
-# also if we need to change how read files, we only need to change the get_book_text function
+# is better to have several functions, following the separation of concerns principle
+# this way, we can test eacht function separately 
+# and we can reuse any function if needed to other purposes
+# this way, we can also make the code more readable and maintainable
+# and we can add more features easily
+
+def count_words(text): # function to count the words in the text
+    return len(text.split()) # split() splits the text into a list of words
+    # len() counts the number of items in the list
 
 main()
