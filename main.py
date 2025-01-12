cd416c3b2d4a839c9e8e468b000eb9ab9ad99e22
justabloob <justabloob@gmail.com>
2025-01-12 19:36:55 +0100

    added comments to fully understand how everything works

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 def main():
-    book_path = "books/frankenstein.txt"
-    text = get_text(book_path)
+    book_path = "books/frankenstein.txt" # stablish the path to the book
+    text = get_book_text(book_path) # get the text from the book
     print(text)
 
-def get_text(book_path):
-    with open(book_path) as f:
-        return f.read()
+def get_book_text(book_path): # function to get the text from the book
+    with open(book_path) as f: # f is our opened file
+        return f.read() # read() gets all the text and returns it
+
+# is better to have two functions, following the separation of concerns principle
+# this way, we can test the get_book_text function separately from the main function
+# and we can reuse the get_book_text function if we need to get the text from another book
+# also if we need to change how read files, we only need to change the get_book_text function
 
 main()
